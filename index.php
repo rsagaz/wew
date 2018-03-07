5a5a49a4ecf31ea1d4f79ff0fdd4a8db200ac3b7
rsagaz <37095388+rsagaz@users.noreply.github.com>
2018-03-07 11:50:32 +0700

    Update index.php

--- index.php
+++ index.php
@@ -63,9 +63,9 @@ $app->post('/', function ($request, $response)
 {
 		if($userMessage == "Tips Fashion"){
 $carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
-  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Niko Chong", "Fashion Designer","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",[
-  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Lihat Yuk',"http://interstudifashion.com/"),]),[
-	  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Cek Yuk',"http://interstudibroadcasting.com/"),
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Niko Chong", "Fashion Designer","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",
+										 [
+  new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Lihat Yuk',"http://interstudifashion.com/") <br/> new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Cek Yuk',"http://interstudibroadcasting.com/"),
   ]),
 	
   new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Adji Notonegoro", "Fashion World","https://i0.wp.com/angryanimebitches.com/wp-content/uploads/2013/03/tamakomarket-overallreview-tamakoanddera.jpg",[
