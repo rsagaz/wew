f1d5e79cada1b1b4a08d0f86fde8f4fe0a080294
rsagaz <37095388+rsagaz@users.noreply.github.com>
2018-03-07 09:59:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -60,7 +60,21 @@ $app->post('/', function ($request, $response)
 		
 		
 	}
-
+{
+		$userMessage = $event['message']['text'];
+		if(strtolower($userMessage) == 'halo')
+			
+		{
+			
+			$message = "Halo apa kabar kk";
+            $textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder($message);
+			$result = $bot->replyMessage($event['replyToken'], $textMessageBuilder);
+			return $result->getHTTPStatus() . ' ' . $result->getRawBody();
+		
+		}
+		
+		
+	}
 
 	
 	
