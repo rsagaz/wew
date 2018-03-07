6b85c5b73e0e60ad5e17806cc87aa83044f28b53
rsagaz <37095388+rsagaz@users.noreply.github.com>
2018-03-07 13:23:55 +0700

    Update index.php

--- index.php
+++ index.php
@@ -79,7 +79,7 @@ return $result->getHTTPStatus() . ' ' . $result->getRawBody();
 		
 		
 	}
-if($userMessage == "hai"){
+if($userMessage == "Hai"){
 $confirmTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\ConfirmTemplateBuilder(
    "Hai Apakah Kamu Ganteng?",
    [
