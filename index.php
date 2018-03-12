b2e2bcf1892b1220eadf87189183603115fdc194
rsagaz <37095388+rsagaz@users.noreply.github.com>
2018-03-12 12:35:11 +0700

    Update index.php

--- index.php
+++ index.php
@@ -51,7 +51,7 @@ $app->post('/', function ($request, $response)
 	{
 		if($userMessage == "Tips Fashion"){
 $carouselTemplateBuilder = new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselTemplateBuilder([
-  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Niko Chong", "Fashion Designer","http://interstudisekretaris.com/wp-content/uploads/2018/03/bl-1.jpg",[
+  new \LINE\LINEBot\MessageBuilder\TemplateBuilder\CarouselColumnTemplateBuilder("Niko Chong", "Fashion Designer","https://interstudisekretaris.com/wp-content/uploads/2018/03/bl-1.jpg",[
   new \LINE\LINEBot\TemplateActionBuilder\UriTemplateActionBuilder('Lihat Yuk',"http://interstudifashion.com/"),
   ]),
 	
